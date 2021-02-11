75600a12631a3beae9f3ca113a17872bc3c6d423
zzcontinent <zzcontinent@163.com>
2021-02-12 01:12:08 +0800

    update bootmain.c

--- src/boot/bootmain.c
+++ src/boot/bootmain.c
@@ -1,1 +1,22 @@
 #include <defs.h>
+
+void bootmain(void)
+{
+
+}
+
+void readseg(uintprt_t va, uint32_t count, uint32_t offset)
+{
+
+}
+
+void readsect(void *dst, uint32_t secno)
+{
+
+}
+
+void waitdisk(void)
+{
+
+}
+
