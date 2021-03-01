66d84580bb98ccf15ec31d958810469a60d023ea
zzcontinent <zzcontinent@163.com>
2021-03-02 01:43:49 +0800

    update libs/x86.h

--- src/libs/x86.h
+++ src/libs/x86.h
@@ -46,6 +46,75 @@ static inline void outsl(uint32_t port, const void* addr, int cnt)
 			: "memory", "cc");
 }
 
+static inline uint32_t read_ebp(void)
+{
+	uint32_t ebp;
+	asm volatile ("movl %%ebp, %0" : "=r" (ebp));
+	return ebp;
+}
+
+static inline read_dr(unsigned regnum)
+{
+	uint32_t value = 0;
+	switch (regnum)
+	{
+		case 0:
+			asm volatile ("movl %%db0, %0" : "=r" (value));
+			break;
+		case 1:
+			asm volatile ("movl %%db1, %0" : "=r" (value));
+			break;
+		case 2:
+			asm volatile ("movl %%db2, %0" : "=r" (value));
+			break;
+		case 3:
+			asm volatile ("movl %%db3, %0" : "=r" (value));
+			break;
+		case 6:
+			asm volatile ("movl %%db6, %0" : "=r" (value));
+			break;
+		case 7:
+			asm volatile ("movl %%db7, %0" : "=r" (value));
+			break;
+	}
+	return value;
+}
+
+static void write_dr(unsigned regnum, uint32_t value)
+{
+	switch (regnum)
+	{
+		case 0:
+			asm volatile ("movl %0, %%db0" :: "r" (value));
+			break;
+		case 1:
+			asm volatile ("movl %0, %%db1" :: "r" (value));
+			break;
+		case 2:
+			asm volatile ("movl %0, %%db2" :: "r" (value));
+			break;
+		case 3:
+			asm volatile ("movl %0, %%db3" :: "r" (value));
+			break;
+		case 6:
+			asm volatile ("movl %0, %%db6" :: "r" (value));
+			break;
+		case 7:
+			asm volatile ("movl %0, %%db7" :: "r" (value));
+			break;
+	}
+}
+
+static inline void breakpoint(void)
+{
+	asm volatile ("int $3");
+}
+
+static inline void lidt(struct pseudodesc * pd)
+{
+	asm volatile ("lidt (%0)" :: "r" (pd) : "memory");
+}
+
 static inline void sti(void)
 {
 	asm volatile ("sti");
@@ -66,6 +135,49 @@ static inline uint32_t read_eflags(void)
 static inline void write_eflags(uint32_t eflags)
 {
 	asm volatile ("pushl %0; popfl" :: "r" (eflags));
+}
+
+static inline void lcr0(uintprt_t cr0)
+{
+	asm volatile ("mov %0, %%cr0" :: "r" (cr0) : "memory");
+}
+
+static inline void lcr3(uintprt_t cr3)
+{
+	asm volatile ("mov %0, %%cr3" :: "r" (cr3) : "memory");
+}
+
+static inline uintprt_t rcr0(void)
+{
+	uintprt_t cr0;
+	asm volatile ("mov %%cr0, %0" : "=r" (cr0) :: "memory");
+	return cr0;
+}
+
+static inline uintprt_t rcr1(void)
+{
+	uintprt_t cr1;
+	asm volatile ("mov %%cr1, %0" : "=r" (cr1) :: "memory");
+	return cr1;
+}
+
+static inline uintprt_t rcr2(void)
+{
+	uintprt_t cr2;
+	asm volatile ("mov %%cr2, %0" : "=r" (cr2) :: "memory");
+	return cr2;
+}
+
+static inline uintprt_t rcr3(void)
+{
+	uintprt_t cr3;
+	asm volatile ("mov %%cr3, %0" : "=r" (cr3) :: "memory");
+	return cr3;
+}
+
+static inline void invlpg(void *addr)
+{
+	asm volatile ("invlpg (%0)" :: "r" (addr) : "memory");
 }
 
 #ifndef __HAVE_ARCH_STRCMP
@@ -91,4 +203,73 @@ static inline int __strcmp(const char *s1, const char* s2)
 }
 #endif /*__HAVE_ARCH_STRCMP*/
 
+#ifndef __HAVE_ARCH_STRCPY
+#define __HAVE_ARCH_STRCPY
+static inline char * __strcpy(char * dst, const char * src)
+{
+	int d0, d1, d2;
+	asm volatile ( "1: lodsb;"
+			"stosb;"
+			"testb %%al, %%al;"
+			"jne 1b;"
+			: "=&S" (d0), "=&D" (d1),"=&D" (d2)
+			: "0" (src), "1" (dst) : "memory"
+		     );
+	return dst;
+}
+#endif /*__HAVE_ARCH_STRCPY*/
+
+#ifndef __HAVE_ARCH_MEMSET
+#define __HAVE_ARCH_MEMSET
+static inline  void * __memset(void *s, char c, size_t n)
+{
+	int d0, d1;
+	asm volatile ("rep; stosb;"
+			: "=&c" (d0), "=&D" (d1)
+			: "0" (n), "a" (c), "1" (s)
+			: "memory");
+	return s;
+}
+#endif /*__HAVE_ARCH_MEMSET*/
+
+#ifndef __HAVE_ARCH_MEMMOVE
+#define __HAVE_ARCH_MEMMOVE
+static inline void * __memmove(void * dst, const void * src, size_t n)
+{
+	if (dst < src)
+	{
+		return __memcpy(dst, src, n);
+	}
+	int d0, d1, d2;
+	asm volatile ("std;"
+			"rep; movsb;"
+			"cld;"
+			: "=&c" (d0), "=&S" (d1), "=&D" (d2)
+			: "0" (n), "1" (n -1 + src), "2" (n - 1 + dst)
+			: "memory");
+	return dst;
+}
+#endif /*__HAVE_ARCH_MEMMOVE*/
+
+#ifdef __HAVE_ARCH_MEMCPY
+#define __HAVE_ARCH_MEMCPY
+__memcpy(void * dst, const void * src, size_t n)
+{
+	int d0, d1, d2;
+	asm volatile (
+			"rep; movsl;"
+			"movl %4, %%ecx;"
+			"movl %3, %%ecx;"
+			"jz 1f;"
+			"rep; movsb;"
+			"1:"
+			: "=&c" (d0), "=&S" (d1), "=&D" (d2)
+			: "0" (n / 4), "g" (n), "1" (dst), "2" (src)
+			: "memory"
+		     );
+	return dst;
+}
+#endif /*__HAVE_ARCH_MEMCPY*/
+
 #endif /*__LIBS_X86_H__*/
+
