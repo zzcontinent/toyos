44a7f1d50f25eed45b11f3c1e281879c8b2e6719
zzcontinent <zzcontinent@163.com>
2021-03-02 19:18:29 +0800

    first make bin/bootblock build

--- src/boot/asm.h
+++ src/boot/asm.h
@@ -9,7 +9,7 @@
     .byte 0, 0, 0, 0
 
 #define SEG_ASM(type, base, limit)                    \
-    .word (((limit) >> 12) & 0xffff), (base)&0xffff); \
+    .word (((limit) >> 12) & 0xffff), (base)&0xffff;  \
     .byte(((base) >> 16) & 0xff), (0x90 | (type)),    \
 	(0xC0 | (((limit) >> 28) & 0xf)), (((base) >> 24) & 0xff)
 
--- src/boot/bootmain.c
+++ src/boot/bootmain.c
@@ -2,25 +2,61 @@
 #include <elf.h>
 
 #define SECTSIZE 512
-#define ELFHDR ((struct elfhdr *)0x10000) // scratch space
-
-void bootmain(void)
-{
-
-}
-
-void readseg(uintprt_t va, uint32_t count, uint32_t offset)
-{
-
-}
-
-void readsect(void *dst, uint32_t secno)
-{
-
-}
+#define ELFHDR ((struct elfhdr*)0x10000) // scratch space
 
 static void waitdisk(void)
 {
-
+    while ((inb(0x1F7) & 0xC0) != 0x40);
 }
 
+/* read_sect -  read a single sector at @secnum into @dst*/
+static void read_sect(void* dst, uint32_t secnum)
+{
+    waitdisk();
+    outb(0x1F2, 1);
+    outb(0x1F3, secnum & 0xFF);
+    outb(0x1F4, (secnum >> 8) & 0xFF);
+    outb(0x1F5, (secnum >> 16) & 0xFF);
+    outb(0x1F6, ((secnum >> 24) & 0xFF) | 0xE0);
+    outb(0x1F7, 20);
+
+    waitdisk();
+    insl(0x1F0, dst, SECTSIZE / 4);
+}
+
+/*
+ * read_seg - read @count bytes at @offset from kernel into virtual address @va,
+ * might copy more than asked
+ * */
+static void read_seg(uintprt_t va, uint32_t count, uint32_t offset)
+{
+    uintprt_t end_va = va + count;
+    va -= offset % SECTSIZE;
+    uint32_t secnum = (offset / SECTSIZE) + 1;
+    for (; va < end_va; va += SECTSIZE, secnum++) {
+	read_sect((void*)va, secnum);
+    }
+}
+
+void bootmain(void)
+{
+    read_seg((uintprt_t)ELFHDR, SECTSIZE * 8, 0);
+    if (ELFHDR->e_magic != ELF_MAGIC) {
+	goto bad;
+    }
+    struct proghdr *ph, *eph;
+    ph = (struct proghdr*)((uintprt_t)ELFHDR + ELFHDR->e_phoff);
+    eph = ph + ELFHDR->e_phnum;
+    for (; ph < eph; ph++) {
+	read_seg(ph->p_va & 0xFFFFFF, ph->p_memsz, ph->p_offset);
+    }
+    // call the entry point from the ELF header
+    // note: does not return
+    ((void (*)(void))(ELFHDR->e_entry & 0xFFFFFF))();
+
+bad:
+    outw(0x8A00, 0x8A00);
+    outw(0x8A00, 0x8E00);
+
+    while (1);
+}
