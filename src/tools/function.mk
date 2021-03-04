80b1bdacbc31957cd7cadb9077698cf7c0a49bf8
zzcontinent <zzcontinent@163.com>
2021-03-05 01:07:55 +0800

    try to distinguish differences between target and packet

--- src/tools/function.mk
+++ src/tools/function.mk
@@ -2,33 +2,22 @@ OBJPREFIX := __objs_
 SLASH := /
 .SECONDEXPANSION:
 
-# 1
 # list all files in some directories
 # (#directories, $types)
 listf = $(filter $(if $(2),$(addprefix %.,$(2)),%), \
 	$(wildcard $(addsuffix $(SLASH)*,$(1))))
 
-# 2
 # get .o obj files
-# (#files[, packet])
-toobj = $(addprefix $(OBJDIR)$(SLASH)$(if $(2),$(2)$(SLASH)),\
-	$(addsuffix .o,$(basename $(1))))
+# (#files)
+toobj = $(addprefix $(OBJDIR)$(SLASH),$(addsuffix .o,$(basename $(1))))
 
-# 3
 # get .d dependency files
-# (#files[,packet])
-todep = $(patsubst %.o,%.d,$(call toobj,$(1),$(2)))
+# (#files)
+todep = $(patsubst %.o,%.d,$(call toobj,$(1)))
 
-# 4
 # add $(BINDIR)/ to $(1)
 tobin = $(addprefix $(BINDIR)$(SLASH),$(1))
 
-# 5
-# change $(name) to $(OBJPREFIX)$(name)
-# (#names)
-packetname = $(if $(1),$(addprefix $(OBJPREFIX),$(1)),$(OBJPREFIX))
-
-# 6
 # cc compile template, generate rule for dep, obj:
 # (file, cc[, flags, dir])
 define cc_template
@@ -41,44 +30,22 @@ $$(call toobj,$(1),$(4)): $(1) | $$$$(dir $$$$@)
 	@$(V)$(2) -I$$(dir $(1)) $(3) -c $$< -o $$@
 endef
 
-# 7
 # compile file
 # (#files, cc[,flags,dir])
-define do_cc_compile
+define cc_template_batch
 $$(foreach f,$(1),$$(eval $$(call cc_template,$$(f),$(2),$(3),$(4))))
 endef
 
-# 8
-# add files to packet
-# (#files, cc[,flags, packet, dir])
-define do_add_files_to_packet
-__temp_packet__ := $$(call packetname,$(4))
-ifeq ($$(origin $$(__temp_packet__)),undefined)
-$$(__temp_packet__) :=
-endif
-__temp_objs__ := $$(call toobj,$(1),$(5))
-$$(foreach f,$(1),$$(eval $$(call cc_template,$$(f),$(2),$(3),$(5))))
-$$(__temp_packet__) += $$(__temp_objs__)
-endef
-
-# 9
-# add objs to packet
-# (#objs, packet)
-define do_add_objs_to_packet
-__temp_packet__ := $(call packetname,$(2))
-ifeq ($$(origin $$(__temp_packet__)),undefined)
-$$(__temp_packet__) :=
-endif
-$$(__temp_packet__) += $(1)
-endef
+# compile file
+# (#files, cc[, flags, dir])
+cc_compile = $(eval $(call cc_template_batch,$(1),$(2),$(3),$(4)))
 
 # 10
-# add packet and objs to target
-# (target, #packets, #objs, cc, [, flags])
-define do_create_target
+# add packet and objs to bin
+# (bin, #packets, #objs, cc, [, flags])
+define do_create_bin
 __temp_bin__ = $$(call tobin,$(1))
 __temp_objs__ = $$(foreach p,$$(call packetname,$(2)),$$($$(p))) $(3)
-TARGETS += $$(__temp_bin__)
 ifneq ($(4),)
 $$(__temp_bin__): $$(__temp_objs__) | $$$$(dir $$$$@)
 	$(V)$(4) $(5) $$^ -o $$@
@@ -96,14 +63,6 @@ $$(sort $$(dir $$(ALLOBJS)) $(BINDIR)$(SLASH) $(OBJDIR)$(SLASH)):
 endef
 
 # 12
-# compile file
-# (#files, cc[, flags, dir])
-cc_compile = $(eval $(call do_cc_compile,$(1),$(2),$(3),$(4)))
-
-# 13
-# add files to packet
-# (#files, cc[, flags, packet, dir])
-add_files = $(eval $(call do_add_files_to_packet,$(1),$(2),$(3),$(4),$(5)))
 
 # 14
 # add objs to packet
@@ -111,9 +70,9 @@ add_files = $(eval $(call do_add_files_to_packet,$(1),$(2),$(3),$(4),$(5)))
 add_objs = $(eval $(call do_add_objs_to_packet,$(1),$(2)))
 
 # 15
-# add packets and objs to target
-# (target, #packets, #objs, cc, [, flags])
-create_target = $(eval $(call do_create_target,$(1),$(2),$(3),$(4),$(5)))
+# add packets and objs to bin
+# (bin, #packets, #objs, cc, [, flags])
+create_bin = $(eval $(call do_create_bin,$(1),$(2),$(3),$(4),$(5)))
 
 read_packet = $(foreach p,$(call packetname,$(1)),$($(p)))
 
