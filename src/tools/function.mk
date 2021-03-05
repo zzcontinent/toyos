38d357dd10bd0ed773b5f07251843b42c06c33ab
zzcontinent <zzcontinent@163.com>
2021-03-05 14:03:07 +0800

    tobin -> to_out_target toobj -> to_out_obj todep -> to_out_dep

--- src/tools/function.mk
+++ src/tools/function.mk
@@ -2,89 +2,83 @@ OBJPREFIX := __objs_
 SLASH := /
 .SECONDEXPANSION:
 
+# -------------------------------------------------------------------------
 # list all files in some directories
 # (#directories, $types)
 listf = $(filter $(if $(2),$(addprefix %.,$(2)),%), \
 	$(wildcard $(addsuffix $(SLASH)*,$(1))))
 
+# (#directories, $types)
+listf_cc = $(call listf,$(1),$(CTYPE))
+
+# -------------------------------------------------------------------------
 # get .o obj files
 # (#files)
-toobj = $(addprefix $(OBJDIR)$(SLASH),$(addsuffix .o,$(basename $(1))))
+to_out_obj = $(addprefix $(OUT_OBJDIR)$(SLASH),$(addsuffix .o,$(basename $(1))))
 
+# -------------------------------------------------------------------------
 # get .d dependency files
 # (#files)
-todep = $(patsubst %.o,%.d,$(call toobj,$(1)))
+to_out_dep = $(patsubst %.o,%.d,$(call to_out_obj,$(1)))
 
-# add $(BINDIR)/ to $(1)
-tobin = $(addprefix $(BINDIR)$(SLASH),$(1))
+# -------------------------------------------------------------------------
+# add $(OUT_TARGETDIR)/ to $(1)
+to_out_target = $(addprefix $(OUT_TARGETDIR)$(SLASH),$(1))
 
+# -------------------------------------------------------------------------
 # cc compile template, generate rule for dep, obj:
-# (file, cc[, flags, dir])
+# (file, cc[, flags])
 define cc_template
-ALLOBJS += $$(call toobj,$(1),$(4))
-$$(call todep,$(1),$(4)): $(1) | $$$$(dir $$$$@)
+ALLOBJS += $$(call to_out_obj,$(1))
+ALLDEPS += $$(call to_out_dep,$(1))
+$$(call to_out_dep,$(1)): $(1) | $$$$(dir $$$$@)
 	@echo + cc deps $$<
 	@$(V)$(2) -I$$(dir $(1)) $(3) -MM $$< -MT "$$(patsubst %.d,%.o,$$@) $$@" > $$@
-$$(call toobj,$(1),$(4)): $(1) | $$$$(dir $$$$@)
+$$(call to_out_obj,$(1)): $(1) | $$$$(dir $$$$@)
 	@echo + cc $$<
 	@$(V)$(2) -I$$(dir $(1)) $(3) -c $$< -o $$@
 endef
 
+# -------------------------------------------------------------------------
 # compile file
-# (#files, cc[,flags,dir])
+# (#files, cc[,flags])
 define cc_template_batch
-$$(foreach f,$(1),$$(eval $$(call cc_template,$$(f),$(2),$(3),$(4))))
+$$(foreach f,$(1),$$(eval $$(call cc_template,$$(f),$(2),$(3))))
 endef
 
+# -------------------------------------------------------------------------
 # compile file
-# (#files, cc[, flags, dir])
-cc_compile = $(eval $(call cc_template_batch,$(1),$(2),$(3),$(4)))
-
-# 10
-# add packet and objs to bin
-# (bin, #packets, #objs, cc, [, flags])
-define do_create_bin
-__temp_bin__ = $$(call tobin,$(1))
-__temp_objs__ = $$(foreach p,$$(call packetname,$(2)),$$($$(p))) $(3)
-ifneq ($(4),)
-$$(__temp_bin__): $$(__temp_objs__) | $$$$(dir $$$$@)
-	$(V)$(4) $(5) $$^ -o $$@
+# (#files, cc[, flags])
+cc_compile_batch = $(eval $(call cc_template_batch,$(1),$(2),$(3)))
+# (#files)
+compile_files = $(call cc_compile_batch,$(1),$(CC),$(CFLAGS))
+# (#files)
+compile_files_host = $(call cc_compile_batch,$(1),$(HOSTCC),$(HOSTCFLAGS))
+# -------------------------------------------------------------------------
+# add packet and objs to target
+# (target, #objs, cc, [, flags])
+define do_create_target
+ifneq ($(3),)
+$$(call to_out_target,$(1)): $$(call to_out_obj,$(2)) | $$$$(dir $$$$@)
+	$(V)$(3) $(4) $$^ -o $$@
 else
-$$(__temp_bin__): $$(__temp_objs__) | $$$$(dir $$$$@)
+$$(call to_out_target,$(1)): $$(call to_out_obj,$(2)) | $$$$(dir $$$$@)
 endif
 endef
 
-# 11
+# (target, #objs, cc, [, flags])
+create_target = $(eval $(call do_create_target,$(1),$(2),$(3),$(4)))
+# (target, #objs)
+create_target_cc = $(call create_target,$(1),$(2),$(CC),$(CFLAGS))
+# (target, #objs)
+create_target_host = $(call create_target,$(1),$(2),$(HOSTCC),$(HOSTCFLAGS))
+# -------------------------------------------------------------------------
 # finish all
 define do_finish_all
-ALLDEPS = $$(ALLOBJS:.o=.d)
-$$(sort $$(dir $$(ALLOBJS)) $(BINDIR)$(SLASH) $(OBJDIR)$(SLASH)):
+$$(sort $$(dir $$(ALLOBJS)) $(OUT_TARGETDIR)$(SLASH) $(OUT_OBJDIR)$(SLASH)):
 	@mkdir -p $$@
 endef
 
-# 12
-
-# 14
-# add objs to packet
-# (#objs, packet)
-add_objs = $(eval $(call do_add_objs_to_packet,$(1),$(2)))
-
-# 15
-# add packets and objs to bin
-# (bin, #packets, #objs, cc, [, flags])
-create_bin = $(eval $(call do_create_bin,$(1),$(2),$(3),$(4),$(5)))
-
-read_packet = $(foreach p,$(call packetname,$(1)),$($(p)))
-
-add_dependency = $(eval $(1): $(2))
-
 finish_all = $(eval $(call do_finish_all))
 
-
-
-
-
-
-
-
-
+# -------------------------------------------------------------------------
