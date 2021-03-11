dca6f811cc2c83503100a405c479622e2d3fcf27
zzcontinent <zzcontinent@163.com>
2021-03-12 01:43:08 +0800

    update printfmt

--- src/libs/printfmt.c
+++ src/libs/printfmt.c
@@ -1,11 +1,9 @@
-#include "stdarg.h"
-#include "trap.h"
 #include <defs.h>
 #include <error.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
-#include <x86.h>
 
 static const char* const error_string[MAXERROR + 1] = {
 	[0] NULL,
@@ -45,6 +43,17 @@ static void printnum((void* putch)(int, void*, int), int fd, void* putdat, unsig
 	}
 	putch("0123456789abcdef"[mod], putdat, fd);
 }
+static long long getint(va_list *ap, int lflag)
+{
+	if (lflag >= 2)
+	{
+		return va_arg(*ap, long long);
+	}else if(lflag){
+		return va_arg(*ap, long);
+	}else{
+		return va_arg(*ap, int);
+	}
+}
 
 static unsigned long long getuint(va_list* ap, int lflag)
 {
@@ -55,6 +64,14 @@ static unsigned long long getuint(va_list* ap, int lflag)
 	} else {
 		return va_arg(*ap, unsigned int);
 	}
+}
+
+void printfmt(void (*putch)(int, void*, int), int fd, void* putdat, const char* *fmt,...)
+{
+	va_list ap;
+	va_start(ap,fmt);
+	vprintfmt(putch, fd, putdat, fmt, ap);
+	va_end(ap);
 }
 
 void vprintfmt(void (*putch)(int, void*, int), int fd, void* putdat, const char* fmt, va_list ap)
@@ -122,6 +139,113 @@ process_precision:
 					printfmt(putch, fd, putdat, "%s", p);
 				}
 				break;
+			case 's':
+				if ((p = va_arg(ap, char*)) == NULL){
+					p = "(null)";
+				}
+				if (width > 0 && padc != '-')
+				{
+					for (width -= strnlen(p, precision);width > 0 ; width --)
+					{
+						putch(padc, putdat, fd);
+					}
+				}
+				for (;(ch = *p++) != '\0' && (precision <0 || --precision >=0);width--)
+				{
+					if (altflag && (ch < ' ' || ch > '~'))
+					{
+						putch('?', putdat, fd);
+					}else{
+						putch(ch, putdat, fd);
+					}
+				}
+				for (;width > 0; width --)
+				{
+					putch(' ', putdat, fd);
+				}
+				break;
+			case 'd':
+				num = getuint(&ap, lflag);
+				if ((long long)num < 0)
+				{
+					putch('-', putdat, fd);
+					num = -(long long)num;
+				}
+				base = 10;
+				goto number;
+			case 'u':
+				num = getuint(&ap, lflag);
+				base = 10;
+				goto number;
+			case 'o':
+				num = getuint(&ap, lflag);
+				base = 8;
+				goto number;
+			case 'p':
+				putch('0', putdat, fd);
+				putch('x', putdat, fd);
+				num = (unsigned long long)(uintptr_t)va_arg(ap, void *);
+				base = 16;
+				goto number;
+			case 'x':
+				num = getuint(&ap, lflag);
+				base =16;
+number:
+				printnum(putch, fd, putdat, num, base, width, padc);
+				break;
+			case '%':
+				putch(ch, putdat, fd);
+				break;
+			default:
+				putch('%', putdat, fd);
+				for (fmt --; fmt[-1] != '%'; fmt--)
+					;
+				break;
 		}
 	}
 }
+
+
+
+struct sprintbuf{
+	char*buf;
+	char* ebuf;
+	int cnt;
+};
+
+static void snprintputch(int ch, struct sprintbuf* b)
+{
+	b->cnt ++;
+	if (b->buf < b->ebuf)
+	{
+		*b->buf ++ = ch;
+	}
+}
+
+
+int snprintf(char* str, size_t size, const char* fmt,...)
+{
+	va_list ap;
+	int cnt;
+	va_start(ap, fmt);
+	cnt = vsnprintf(str, size, fmt, ap);
+	va_end(ap);
+	return cnt;
+}
+
+int vsnprintf(char* str, size_t size, const char* fmt, va_list ap)
+{
+	struct sprintbuf b = {str, str+size -1, 0};
+	if (str ==NULL || b.buf > b.ebuf)
+	{
+		return -E_INVAL;
+	}
+	vprintfmt((void*) snprintputch, NO_FD, &b, fmt, ap);
+	*b.buf = '\0';
+	return b.cnt;
+}
+
+
+
+
+
