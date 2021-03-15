ed749564439127d95574496abf7c8002d5081d81
zzcontinent <zzcontinent@163.com>
2021-03-15 19:45:36 +0800

    add pmm .h .c

---
+++ src/kern/mm/pmm.c
@@ -0,0 +1,13 @@
+#include <pmm.h>
+#include <vmm.h>
+#include <kmalloc.h>
+#include <defs.h>
+#include <x86.h>
+#include <stdio.h>
+#include <string.h>
+#include <memlayout.h>
+#include <mmu.h>
+#include <default_pmm.h>
+
+
+
