84509617eba9f4b5d326aa20e3577a6622b81a6a
zzcontinent <zzcontinent@163.com>
2021-03-16 11:15:34 +0800

    update ..

--- src/kern/mm/pmm.h
+++ src/kern/mm/pmm.h
@@ -45,7 +45,7 @@ void tlb_invalidate(pde_t* pgdir, uintptr_t la);
 struct Page* pgdir_alloc_page(pde_t* pgdir, uintptr_t la, uint32_t perm);
 void unmap_range(pde_t* pgdir, uintptr_t start, uintptr_t end);
 void exit_range(pde_t* pgdir, uintptr_t start, uintptr_t end);
-int copy_range(pde_t* to, pde_t* from, uintptr_t start, uintptr_t end, bool, share);
+int copy_range(pde_t* to, pde_t* from, uintptr_t start, uintptr_t end, bool, bool share);
 
 void print_pgdir(void);
 
