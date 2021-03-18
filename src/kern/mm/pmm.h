9d930d2c197120ad77d395887cd07ac28b7f41c1
zzcontinent <zzcontinent@163.com>
2021-03-18 17:32:44 +0800

    pmm

--- src/kern/mm/pmm.h
+++ src/kern/mm/pmm.h
@@ -9,13 +9,13 @@
 #include <mmu.h>
 
 struct pmm_manager {
-    const char* name;
-    void (*init)(void);
-    void (*init_memmap)(struct Page* base, size_t n);
-    struct Page* (*alloc_pages)(size_t n);
-    void (*free_pages)(struct Page* base, size_t n);
-    size_t (*nr_free_pages)(void);
-    void (*check)(void);
+	const char* name;
+	void (*init)(void);
+	void (*init_memmap)(struct Page* base, size_t n);
+	struct Page* (*alloc_pages)(size_t n);
+	void (*free_pages)(struct Page* base, size_t n);
+	size_t (*nr_free_pages)(void);
+	void (*check)(void);
 };
 
 extern const struct pmm_manager* pmm_manager;
@@ -54,77 +54,83 @@ void print_pgdir(void);
  * returns the corresponding physical address.
  * */
 #define PADDR(kva) (                                               \
-    {                                                              \
-	uintptr_t __m_kva = (uintptr_t)(kva);                      \
-	if (__m_kva < KERNBASE) {                                  \
-	    panic("PADDR called with invalid kva %08lx", __m_kva); \
-	}                                                          \
-	__m_kva - KERNBASE;                                        \
-    })
+		{                                                              \
+		uintptr_t __m_kva = (uintptr_t)(kva);                      \
+		if (__m_kva < KERNBASE) {                                  \
+		panic("PADDR called with invalid kva %08lx", __m_kva); \
+		}                                                          \
+		__m_kva - KERNBASE;                                        \
+		})
 
 /*
  * takes a physical address
  * returns the corresponding kernel virtual address
  * */
 #define KADDR(pa) ({                                         \
-    uintptr_t __m_pa = (pa);                                 \
-    size_t __m_ppn = PPN(__m_pa);                            \
-    if (__m_ppn >= npage) {                                  \
-	panic("KADDR called with invalid pa %08lx", __m_pa); \
-    }                                                        \
-    (void*)(__m_pa + KERNBASE);                              \
-})
+		uintptr_t __m_pa = (pa);                                 \
+		size_t __m_ppn = PPN(__m_pa);                            \
+		if (__m_ppn >= npage) {                                  \
+		panic("KADDR called with invalid pa %08lx", __m_pa); \
+		}                                                        \
+		(void*)(__m_pa + KERNBASE);                              \
+		})
 
 static inline ppn_t page2ppn(struct Page* page)
 {
-    return page - pages;
+	return page - pages;
 }
 
 static inline uintptr_t page2pa(struct Page* page)
 {
-    return page2ppn(page) << PGSHIFT;
+	return page2ppn(page) << PGSHIFT;
 }
 
 static inline struct Page* pa2page(uintptr_t pa)
 {
-    if (PPN(pa) >= npage) {
-	panic("pa2page called with invalid pa");
-    }
-    return &pages[PPN(pa)];
+	if (PPN(pa) >= npage) {
+		panic("pa2page called with invalid pa");
+	}
+	return &pages[PPN(pa)];
 }
 
 static inline void* page2kva(struct Page* page)
 {
-    return KADDR(page2pa(page));
+	return KADDR(page2pa(page));
 }
 
 static inline struct Page* pte2page(pte_t pte)
 {
-    if (!(pte & PTE_P)) {
-	panic("pte2page call with invalid pte");
-    }
-    return pa2page(PTE_ADDR(pte));
+	if (!(pte & PTE_P)) {
+		panic("pte2page call with invalid pte");
+	}
+	return pa2page(PTE_ADDR(pte));
 }
 
 static inline struct Page* pde2page(pde_t pde)
 {
-    return pa2page(PDE_ADDR(pde));
+	return pa2page(PDE_ADDR(pde));
 }
 
 static inline int page_ref(struct Page* page)
 {
-    return page->ref;
+	return page->ref;
 }
 
 static inline void set_page_ref(struct Page* page, int val)
 {
-    page->ref = val;
+	page->ref = val;
 }
 
 static inline int page_ref_inc(struct Page* page)
 {
-    page->ref += 1;
-    return page->ref;
+	page->ref += 1;
+	return page->ref;
+}
+
+static inline int page_ref_dec(struct Page* page)
+{
+	page->ref -= 1;
+	return page->ref;
 }
 
 #endif /* __KERN_MM_PMM_H__ */
