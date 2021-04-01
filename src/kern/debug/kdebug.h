cf17eee9a36759b344f2be153f91e04e08f746d4
zzcontinent <zzcontinent@163.com>
2021-04-01 19:41:55 +0800

    update include

--- src/kern/debug/kdebug.h
+++ src/kern/debug/kdebug.h
@@ -2,8 +2,10 @@
 #define __KERN_DEBUG_KDEBUG_H__
 
 #include "stdio.h"
+#include "trap.h"
 #include <defs.h>
 
 void print_kerninfo(void);
+void print_stackframe(void);
 
 #endif /* __KERN_DEBUG_KDEBUG_H__ */
