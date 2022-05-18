a06689b2a73470c5512b9b34dc08f57e32ac1b67
zzcontinent <zzcontinent@163.com>
2022-05-18 16:24:22 +0800

    update kdebug.h

--- src/kern/debug/kdebug.h
+++ src/kern/debug/kdebug.h
@@ -5,6 +5,12 @@
 #include "trap.h"
 #include <defs.h>
 
+static union { char c[4]; unsigned long l; } endian_test = { { 'l', '?', '?', 'b' } };
+#define ENDIANNESS ((char)endian_test.l)
+#define L2B32(little)  (((little&0xff)<<24) | ((little&0xff00)<<8) | ((little&0xff0000)>>8) | ((little&0xff000000)>>24))
+
+
+
 void print_kerninfo(void);
 void print_stackframe(void);
 
