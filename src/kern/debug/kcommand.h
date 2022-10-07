7be6716110e34d40d01dfd08f95fca8f07dab8bc
zzcontinent <zzcontinent@163.com>
2022-10-07 18:14:19 +0800

    add cmd_mem

--- src/kern/debug/kcommand.h
+++ src/kern/debug/kcommand.h
@@ -1,6 +1,7 @@
 #ifndef  __KCOMMAND_H__
 #define  __KCOMMAND_H__
 #include <kern/trap/trap.h>
+#include <kern/mm/pmm.h>
 
 void kcmd_loop();
 int cmd_help(int argc, char **argv);
@@ -8,6 +9,7 @@ int cmd_kerninfo(int argc, char **argv);
 int cmd_backtrace(int argc, char **argv);
 int cmd_exit(int argc, char **argv);
 int cmd_jump(int argc, char **argv);
+int cmd_mem(int argc, char **argv);
 
 #define KCMD_LOOP { \
 	uinfo(""); \
