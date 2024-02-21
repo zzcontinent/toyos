aba9a3ff2c14744d1296b1481b102f3168c48094
zzcontinent <zzcontinent@163.com>
2024-02-22 01:00:46 +0800

    [up] log.h with LEVEL_MASK [TODO] sys_writev

--- src/libs/log.h
+++ src/libs/log.h
@@ -19,65 +19,22 @@
 // ============================================================wait if
 
 // ============================================================log
-#define LEVEL_DEBUG  0x1
-#define LEVEL_INFO   0x2
-#define LEVEL_ERROR  0x4
-#define LEVEL_TEST   0x8
-#define LEVEL_OFF    0x10
-//#define ULOG_LEVEL LEVEL_DEBUG
-//#define ULOG_LEVEL LEVEL_INFO
-//#define ULOG_LEVEL LEVEL_ERROR
-//#define ULOG_LEVEL LEVEL_TEST
-//#define ULOG_LEVEL LEVEL_OFF
+#define LEVEL_DEBUG  1
+#define LEVEL_INFO   2
+#define LEVEL_ERROR  4
+#define LEVEL_TEST   8
 
-//#define ULOG_LEVEL (LEVEL_INFO | LEVEL_TEST)
-#define ULOG_LEVEL (LEVEL_INFO)
+#define ULOG_LEVEL_MASK (LEVEL_INFO|LEVEL_ERROR)
 
 
 extern int cprintf(const char* fmt, ...);
 #define printf cprintf
+#define ulog(level, fmt, args...) do { if (ULOG_LEVEL_MASK & level) {printf("[L%x][%s:%d][%s] " fmt, level, __FILE__, __LINE__, __FUNCTION__, ##args);} } while(0)
 
-#if ULOG_LEVEL & LEVEL_OFF
-#define udebug(fmt, args...)
-#define uinfo(fmt, args...)
-#define uerror(fmt, args...)
-#define uclean(fmt, args...)
-#define uonly(fmt, args...)
-#endif
-
-#if ULOG_LEVEL & LEVEL_DEBUG
-#define udebug(fmt, args...) printf("[D][%s:%d][%s] " fmt, __FILE__, __LINE__, __FUNCTION__, ##args)
-#define uinfo(fmt, args...) printf("[I][%s:%d][%s] " fmt,  __FILE__, __LINE__, __FUNCTION__, ##args)
-#define uerror(fmt, args...) printf("[E][%s:%d][%s] " fmt, __FILE__, __LINE__, __FUNCTION__, ##args)
-#endif
-
-#if ULOG_LEVEL & LEVEL_INFO
-#define uinfo(fmt, args...) printf("[I][%s:%d][%s] " fmt, __FILE__, __LINE__, __FUNCTION__, ##args)
-#define uerror(fmt, args...) printf("[E][%s:%d][%s] " fmt, __FILE__, __LINE__, __FUNCTION__, ##args)
-#endif
-
-#if ULOG_LEVEL & LEVEL_ERROR
-#define uerror(fmt, args...) printf("[E][%s:%d][%s] " fmt, __FILE__, __LINE__, __FUNCTION__, ##args)
-#endif
-
-#if ULOG_LEVEL & LEVEL_TEST
-#define utest(fmt, args...) printf("[T][%s:%d][%s] " fmt, __FILE__, __LINE__, __FUNCTION__, ##args)
-#endif
-
-#ifndef udebug
-#define udebug(fmt, args...)
-#endif
-#ifndef uinfo
-#define uinfo(fmt, args...)
-#endif
-#ifndef uerror
-#define uerror(fmt, args...)
-#endif
-#ifndef uclean
-#define uclean printf
-#endif
-#ifndef utest
-#define utest(fmt, args...)
-#endif
+#define udebug(fmt, args...)  ulog(LEVEL_DEBUG, fmt, ##args)
+#define uinfo(fmt, args...)   ulog(LEVEL_INFO, fmt, ##args)
+#define uerror(fmt, args...)  ulog(LEVEL_ERROR, fmt, ##args)
+#define uclean(fmt, args...)  printf(fmt, ##args)
+#define utest(fmt, args...)   ulog(LEVEL_TEST, fmt, ##args)
 
 #endif  /* __LOG_H__ */
